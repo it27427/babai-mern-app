b432b18360ca34436cca51403a9143265611a943
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-03 10:30:43 +0600

    config and db file created, config done and vite.config updated

--- views/vite.config.js
+++ views/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      public: `${path.resolve(__dirname, './public/')}`,
+      pages: path.resolve(__dirname, './src/pages'),
+      layouts: `${path.resolve(__dirname, './src/layouts')}`,
+      assets: `${path.resolve(__dirname, './src/assets')}`,
+    },
+  },
+});

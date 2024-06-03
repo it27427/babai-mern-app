ccbee87b6ebb94963dae66195a69a95b1e3dd2e9
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-03 12:53:37 +0600

    pages added in app component

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -1,8 +1,57 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import About from '@/pages/About';
+import Cart from '@/pages/Cart';
+import Categories from '@/pages/Categories';
+import Checkout from '@/pages/Checkout';
+import Contact from '@/pages/Contact';
+import Error from '@/pages/Error';
+import Home from '@/pages/Home';
+import Privacy from '@/pages/Privacy';
+import ProductDetails from '@/pages/ProductDetails';
+import Profile from '@/pages/Profile';
+import Terms from '@/pages/Terms';
+import Wishlist from '@/pages/Wishlist';
+
+// AUTHENTICATIONS
+import ForgotPassword from '@/pages/auth/ForgotPassword';
+import Login from '@/pages/auth/Login';
+import Register from '@/pages/auth/Register';
+import ResetPassword from '@/pages/auth/ResetPassword';
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello From Babai</h1>
-    </div>
+    <Router>
+      <Routes>
+        {/* AUTHENTICATIONS */}
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/search-results' element={<Categories />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/wishlist' element={<Wishlist />} />
+        <Route path='/product-details' element={<ProductDetails />} />
+
+        {/* USER-PROFILE */}
+        <Route path='/profile' element={<Profile />} />
+
+        {/* PRIVACY-POLICY & TERMS-AND-CONDITIONS */}
+        <Route path='/terms-conditions' element={<Terms />} />
+        <Route path='/privacy-policy' element={<Privacy />} />
+
+        {/* PAGE-NOT-FOUND */}
+        <Route path='/*' element={<Error />} />
+      </Routes>
+
+      <ToastContainer />
+    </Router>
   );
 };
 
--- views/src/main.jsx
+++ views/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// IMPORT-CSS
-import './assets/css/custom.css';
-import './assets/css/index.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import '@/assets/css/index.css';
+
+import '@/assets/css/custom.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

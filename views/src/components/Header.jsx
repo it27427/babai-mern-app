95e88e117ff8fe18922e8418c29b5775bd7d39ee
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-04 10:25:06 +0600

    Header Height Fixed

--- views/src/components/Header.jsx
+++ views/src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar';
 
 const Header = () => {
   return (
-    <header className='sticky top-0 left-0 w-full z-50 bg-slate-950 shadow-md'>
+    <header className='sticky top-0 left-0 w-full z-50 bg-slate-950 shadow-md h-16 flex items-center'>
       <div className='container'>
         <Navbar />
       </div>

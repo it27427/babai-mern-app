65e44dab1b29b54c4f9c0ddbb68dc62b9279f7a7
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-04 11:03:13 +0600

    Terms Layout Added

--- views/src/pages/Terms.jsx
+++ views/src/pages/Terms.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout';
 
 const Terms = () => {
   return (
-    <div>
+    <RootLayout>
       <h1>Terms and Conditions Page</h1>
-    </div>
+    </RootLayout>
   );
 };
 

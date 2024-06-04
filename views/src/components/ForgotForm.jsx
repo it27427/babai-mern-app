d1ca6f939163951441368356b4c9fe1649bdbd20
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-04 12:46:13 +0600

    Forgot Password Page Completed

--- views/src/components/ForgotForm.jsx
+++ views/src/components/ForgotForm.jsx
@@ -1,5 +1,36 @@
+import Button from '@/components/Button';
+import Form from '@/components/Form';
+import InputField from '@/components/InputField';
+
 const ForgotForm = () => {
-  return <div></div>;
+  return (
+    <Form className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-2'>
+        <label htmlFor='email'>
+          Email <span className='text-red-500 font-bold'>*</span>
+        </label>
+        <InputField
+          type='email'
+          id='email'
+          name='email'
+          placeholder='Enter E-mail Address'
+          className='input input-bordered w-full'
+          // onChange={(e) => }
+          // value={}
+          required={true}
+        />
+      </div>
+
+      <div className='mt-3'>
+        <Button
+          type='submit'
+          className='btn btn-primary uppercase font-semibold w-full transition hover:bg-indigo-800'
+        >
+          Continue
+        </Button>
+      </div>
+    </Form>
+  );
 };
 
 export default ForgotForm;
--- views/src/pages/auth/ForgotPassword.jsx
+++ views/src/pages/auth/ForgotPassword.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+
+import DefaultLayout from '@/layouts/DefaultLayout';
+
+import Logo from '@/assets/images/logo/authlogo.svg';
+import ForgotForm from '@/components/ForgotForm';
 
 const ForgotPassword = () => {
   return (
-    <div>
-      <h1>Forgot Password Page</h1>
-    </div>
+    <DefaultLayout>
+      <div className='flex flex-col gap-5 p-6'>
+        <div className='flex flex-col gap-3 items-center justify-center'>
+          <Link to='/'>
+            <figure className='w-24 h-20 flex items-center justify-center'>
+              <img
+                src={Logo}
+                alt='BRAND-LOGO'
+                className='w-full h-full object-cover'
+              />
+            </figure>
+          </Link>
+          <h1 className='uppercase font-bold text-2xl'>Forgot Password</h1>
+        </div>
+
+        <div className='w-full md:max-w-md mx-auto bg-white shadow-lg border border-slate-200 p-4 md:p-6 rounded'>
+          <ForgotForm />
+        </div>
+
+        <p className='text-center font-medium text-slate-700'>
+          Password Remembered?{' '}
+          <Link
+            to='/login'
+            className='font-semibold text-indigo-400 underline transition hover:text-indigo-600 hover:no-underline'
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+    </DefaultLayout>
   );
 };
 

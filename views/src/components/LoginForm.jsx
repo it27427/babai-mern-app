984fbe6d948b0f342de33d352265a3623ea14081
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-04 12:36:50 +0600

    Register Form

--- views/src/components/LoginForm.jsx
+++ views/src/components/LoginForm.jsx
@@ -6,17 +6,33 @@ import InputField from '@/components/InputField';
 
 const LoginForm = ({}) => {
   return (
-    <>
-      <Form className='flex flex-col gap-4'>
+    <Form className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-2'>
+        <label htmlFor='email'>
+          Email <span className='text-red-500 font-bold'>*</span>
+        </label>
+        <InputField
+          type='email'
+          id='email'
+          name='email'
+          placeholder='Enter E-mail Address'
+          className='input input-bordered w-full'
+          // onChange={(e) => }
+          // value={}
+          required={true}
+        />
+      </div>
+
+      <div className='flex flex-col gap-1'>
         <div className='flex flex-col gap-2'>
-          <label htmlFor='email'>
-            Email <span className='text-red-500 font-bold'>*</span>
+          <label htmlFor='password'>
+            Password <span className='text-red-500 font-bold'>*</span>
           </label>
           <InputField
-            type='email'
-            id='email'
-            name='email'
-            placeholder='Enter E-mail Address'
+            type='password'
+            id='password'
+            name='password'
+            placeholder='Enter Password'
             className='input input-bordered w-full'
             // onChange={(e) => }
             // value={}
@@ -24,43 +40,25 @@ const LoginForm = ({}) => {
           />
         </div>
 
-        <div className='flex flex-col gap-1'>
-          <div className='flex flex-col gap-2'>
-            <label htmlFor='password'>
-              Password <span className='text-red-500 font-bold'>*</span>
-            </label>
-            <InputField
-              type='password'
-              id='password'
-              name='password'
-              placeholder='Enter Password'
-              className='input input-bordered w-full'
-              // onChange={(e) => }
-              // value={}
-              required={true}
-            />
-          </div>
-
-          <div className='flex justify-end'>
-            <Link
-              to='/forgot-password'
-              className='text-sm font-semibold transition hover:text-primary'
-            >
-              Forgot Password?
-            </Link>
-          </div>
-        </div>
-
-        <div className='mt-3'>
-          <Button
-            type='submit'
-            className='btn btn-primary uppercase font-semibold w-full transition hover:bg-indigo-800'
+        <div className='flex justify-end'>
+          <Link
+            to='/forgot-password'
+            className='text-sm font-semibold transition hover:text-primary'
           >
-            Login
-          </Button>
+            Forgot Password?
+          </Link>
         </div>
-      </Form>
-    </>
+      </div>
+
+      <div className='mt-3'>
+        <Button
+          type='submit'
+          className='btn btn-primary uppercase font-semibold w-full transition hover:bg-indigo-800'
+        >
+          Login
+        </Button>
+      </div>
+    </Form>
   );
 };
 
--- views/src/components/RegisterForm.jsx
+++ views/src/components/RegisterForm.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import Button from '@/components/Button';
+import Form from '@/components/Form';
+import InputField from '@/components/InputField';
 
 const RegisterForm = () => {
-  return <div></div>;
+  return;
+  <Form className='flex flex-col gap-4'>
+    <div className='flex flex-col gap-2'>
+      <label htmlFor='email'>
+        Email <span className='text-red-500 font-bold'>*</span>
+      </label>
+      <InputField
+        type='email'
+        id='email'
+        name='email'
+        placeholder='Enter E-mail Address'
+        className='input input-bordered w-full'
+        // onChange={(e) => }
+        // value={}
+        required={true}
+      />
+    </div>
+
+    <div className='flex flex-col gap-2'>
+      <label htmlFor='password'>
+        Password <span className='text-red-500 font-bold'>*</span>
+      </label>
+      <InputField
+        type='password'
+        id='password'
+        name='password'
+        placeholder='Enter Password'
+        className='input input-bordered w-full'
+        // onChange={(e) => }
+        // value={}
+        required={true}
+      />
+    </div>
+
+    <div className='mt-3'>
+      <Button
+        type='submit'
+        className='btn btn-primary uppercase font-semibold w-full transition hover:bg-indigo-800'
+      >
+        Register
+      </Button>
+    </div>
+  </Form>;
 };
 
 export default RegisterForm;

a1a30bf089d37580c42b7d381f5fa451f59a364b
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-04 10:22:32 +0600

    BrandLogo Image Classes Added

--- views/src/components/BrandLogo.jsx
+++ views/src/components/BrandLogo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const BrandLogo = () => {
   return (
     <Link to='/' className='w-28 h-16 flex items-center justify-center'>
-      <img src={Logo} alt='BRAND-LOGO' />
+      <img src={Logo} alt='BRAND-LOGO' className='w-full h-full object-cover' />
     </Link>
   );
 };

86128fc4fea3a60cd63541243b8826e9f27c6526
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-05 10:16:08 +0600

    NotFound Component Completed and Error Page Done

--- views/src/components/NotFound.jsx
+++ views/src/components/NotFound.jsx
@@ -1,17 +1,24 @@
-import notfound from '@/assets/images/notfound/not-found.svg';
+import { BsArrowLeft } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+
+import notfound from '@/assets/images/notfound/not-found.svg';
 
 const NotFound = () => {
   return (
     <div className='flex flex-col items-center justify-center gap-5'>
-      <figure className='flex items-center justify-center w-80 h-80'>
+      <figure className='flex items-center justify-center w-72 h-72 md:w-80 md:h-80'>
         <img
           src={notfound}
           alt='NOT-FOUND-THUMB'
           className='w-ful h-full object-cover'
         />
       </figure>
-      <Link to='/' className='btn btn-primary'>
+
+      <Link
+        to='/'
+        className='btn btn-outline btn-neutral btn-wide text-xs uppercase'
+      >
+        <BsArrowLeft className='mt-0.5' />
         Back to Home
       </Link>
     </div>
